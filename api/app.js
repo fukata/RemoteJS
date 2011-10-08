cfc28a11f84f240c3dafd6b72c198ecaabbbd9f8
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-10-09 02:49:56 +0900

    add api

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@
  */
 
 var express = require('express');
+var httpsubr = require('./httpsubr');
 
 var app = module.exports = express.createServer();
 
@@ -34,5 +35,34 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(3000);
+app.post('/js', function(req, res){
+  var url = '';
+  var js = '';
+  if (typeof req.body != 'undefined') {
+    url = (typeof req.body.url != 'undefined') ? req.body.url : '';
+    js = (typeof req.body.js != 'undefined') ? req.body.js : '';
+  }
+
+  if (!url || !js) {
+    res.json({});
+  } else {
+    httpsubr.post({
+		//uri: 'http://localhost:1975/execute_js', 
+		uri: 'http://local.jsonserver:1975/execute_js', 
+		requestBody: {
+			url: url,
+			js: js,
+		}
+	}, function(err, response, buf) {
+		if (err) {
+			res.json(err);
+		} else {
+			res.json(response);
+		}
+	});
+  }
+});
+
+var port = process.argv[2] || 3000;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
